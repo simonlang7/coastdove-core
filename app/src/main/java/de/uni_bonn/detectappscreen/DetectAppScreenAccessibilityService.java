d13b1d24bf4e86b6a50f6db4eb646d5a85559402
Simon Lang <simon.lang7@gmail.com>
2016-03-23 17:05:04 +0100

    Removed useless files, fixed appscreen detection demo

--- app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
@@ -14,52 +14,54 @@ import java.util.List;
 public class DetectAppScreenAccessibilityService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        if (event.getPackageName().toString().equalsIgnoreCase("com.whatsapp")) {
-            if (event.getSource() != null) {
-                AccessibilityNodeInfo rootNodeInfo = event.getSource();
-                AccessibilityNodeInfo androidIdContent = null;
-                while (rootNodeInfo.getParent() != null) {
-                    if (rootNodeInfo.getViewIdResourceName().equals("android:id/content")) {
-                        Log.i("WDebug", "got id/content.");
-                        androidIdContent = rootNodeInfo;
-                        break;
-                    }
-                    rootNodeInfo = rootNodeInfo.getParent();
-                }
-                Log.i("WDebug", "got root node.");
-
-                if (androidIdContent == null) {
-                    List<AccessibilityNodeInfo> nodeInfos = new LinkedList<>();
-                    nodeInfos.add(rootNodeInfo);
-                    while (androidIdContent == null && nodeInfos.size() > 0) {
-                        AccessibilityNodeInfo currentNodeInfo = nodeInfos.get(0);
-                        if (currentNodeInfo.getViewIdResourceName().equals("android:id/content")) {
-                            androidIdContent = currentNodeInfo;
+        if (event.getPackageName() != null) {
+            if (event.getPackageName().toString().equalsIgnoreCase("com.whatsapp")) {
+                if (event.getSource() != null) {
+                    AccessibilityNodeInfo rootNodeInfo = event.getSource();
+                    AccessibilityNodeInfo androidIdContent = null;
+                    while (rootNodeInfo.getParent() != null) {
+                        if (rootNodeInfo.getViewIdResourceName() != null &&
+                            rootNodeInfo.getViewIdResourceName().equals("android:id/content")) {
+                            Log.i("WDebug", "got id/content.");
+                            androidIdContent = rootNodeInfo;
+                            break;
                         }
-                        else {
-                            for (int i = 0; i < currentNodeInfo.getChildCount(); ++i)
-                                nodeInfos.add(currentNodeInfo.getChild(i));
+                        rootNodeInfo = rootNodeInfo.getParent();
+                    }
+                    Log.i("WDebug", "got root node.");
+
+                    if (androidIdContent == null) {
+                        List<AccessibilityNodeInfo> nodeInfos = new LinkedList<>();
+                        nodeInfos.add(rootNodeInfo);
+                        while (androidIdContent == null && nodeInfos.size() > 0) {
+                            AccessibilityNodeInfo currentNodeInfo = nodeInfos.get(0);
+                            if (currentNodeInfo.getViewIdResourceName() != null
+                                    && currentNodeInfo.getViewIdResourceName().equals("android:id/content")) {
+                                androidIdContent = currentNodeInfo;
+                            } else {
+                                for (int i = 0; i < currentNodeInfo.getChildCount(); ++i)
+                                    nodeInfos.add(currentNodeInfo.getChild(i));
+                            }
+                            nodeInfos.remove(0);
                         }
-                        nodeInfos.remove(0);
                     }
-                }
 
-                if (androidIdContent != null) {
-                    Log.i("WDebug", "...finally got id/content.");
-                    if (androidIdContent.getChildCount() > 0) {
-                        if (androidIdContent.getChild(0).getChildCount() == 1)
-                            Log.i("WDebug", "On super2 awesome Mainscreen!");
-                        else if (androidIdContent.getChild(0).getChildCount() == 2)
-                            Log.i("WDebug", "On super2 awesome Conversation screen!");
-                        else
-                            Log.i("WDebug", "On unknown screen");
+                    if (androidIdContent != null) {
+                        Log.i("WDebug", "...finally got id/content.");
+                        if (androidIdContent.getChildCount() > 0) {
+                            if (androidIdContent.getChild(0).getChildCount() == 1)
+                                Log.i("WDebug", "On super2 awesome Mainscreen!");
+                            else if (androidIdContent.getChild(0).getChildCount() == 2)
+                                Log.i("WDebug", "On super2 awesome Conversation screen!");
+                            else
+                                Log.i("WDebug", "On unknown screen");
+                        } else
+                            Log.i("WDebug", "On another unknown screen");
+
+
+                        //event.getSource().getParent().
+                        //Log.i("WDebug", )
                     }
-                    else
-                        Log.i("WDebug", "On another unknown screen");
-
-
-                    //event.getSource().getParent().
-                    //Log.i("WDebug", )
                 }
             }
         }
