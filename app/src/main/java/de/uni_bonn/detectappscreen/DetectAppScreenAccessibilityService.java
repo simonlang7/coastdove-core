3ccdc7615311478081219117b6ef38f776f37ee5
Simon Lang <simon.lang7@gmail.com>
2016-03-30 18:04:42 +0200

    Update

--- app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
@@ -1,17 +1,59 @@
 package de.uni_bonn.detectappscreen;
 
 import android.accessibilityservice.AccessibilityService;
+import android.os.Environment;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by simon on 20/01/16.
  */
 public class DetectAppScreenAccessibilityService extends AccessibilityService {
+
+    protected String compareScreenWithJSONObject(AccessibilityNodeInfo androidIdContent) {
+        if (androidIdContent.getChildCount() < 1)
+            return "Unknown screen";
+
+        JSONObject jsonObject = InfoActivity.getJSONObject();
+        AccessibilityNodeInfo screenRoot = androidIdContent.getChild(0);
+
+        // Breadth-first search
+        Queue<JSONObject> jsonQueue = new LinkedList<>();
+        Queue<AccessibilityNodeInfo> nodeInfoQueue = new LinkedList<>();
+        jsonQueue.add(jsonObject);
+        nodeInfoQueue.add(screenRoot);
+
+        while (!nodeInfoQueue.isEmpty()) {
+            JSONObject currentJSONObject = jsonQueue.remove();
+            AccessibilityNodeInfo currentNodeInfo = nodeInfoQueue.remove();
+
+            try {
+                JSONArray children = currentJSONObject.getJSONArray("children");
+                if (children.length() != currentNodeInfo.getChildCount())
+                    return "Unknown screen (children)";
+
+
+                for (int i = 0; i < children.length(); ++i) {
+                    // compare
+                }
+            } catch (JSONException e) {
+                Log.e("WDebug", "Error traversing the nodeInfo tree: " + e.getMessage());
+            }
+        }
+
+        return "Unknown screen";
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         if (event.getPackageName() != null) {
@@ -47,16 +89,19 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
                     }
 
                     if (androidIdContent != null) {
-                        Log.i("WDebug", "...finally got id/content.");
-                        if (androidIdContent.getChildCount() > 0) {
-                            if (androidIdContent.getChild(0).getChildCount() == 1)
-                                Log.i("WDebug", "On super2 awesome Mainscreen!");
-                            else if (androidIdContent.getChild(0).getChildCount() == 2)
-                                Log.i("WDebug", "On super2 awesome Conversation screen!");
-                            else
-                                Log.i("WDebug", "On unknown screen");
-                        } else
-                            Log.i("WDebug", "On another unknown screen");
+                        compareScreenWithJSONObject(androidIdContent);
+
+
+//                        Log.i("WDebug", "...finally got id/content.");
+//                        if (androidIdContent.getChildCount() > 0) {
+//                            if (androidIdContent.getChild(0).getChildCount() == 1)
+//                                Log.i("WDebug", "On super2 awesome Mainscreen!");
+//                            else if (androidIdContent.getChild(0).getChildCount() == 2)
+//                                Log.i("WDebug", "On super2 awesome Conversation screen!");
+//                            else
+//                                Log.i("WDebug", "On unknown screen");
+//                        } else
+//                            Log.i("WDebug", "On another unknown screen");
 
 
                         //event.getSource().getParent().
--- app/src/main/java/de/uni_bonn/detectappscreen/InfoActivity.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/InfoActivity.java
@@ -1,16 +1,30 @@
 package de.uni_bonn.detectappscreen;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class InfoActivity extends AppCompatActivity {
+
+    protected static JSONObject jsonObject = null;
+
+    public static JSONObject getJSONObject() {
+        return jsonObject;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,27 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
 
+
+        File testJson = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity.json");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (InputStream is = new FileInputStream(testJson);
+             InputStreamReader isr = new InputStreamReader(is);
+             BufferedReader br = new BufferedReader(isr)) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+            InfoActivity.jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (FileNotFoundException e) {
+            Log.e("WDebug", "File not found: " + testJson.getAbsolutePath());
+            Log.e("WDebug", e.getMessage());
+        } catch (JSONException e) {
+            Log.e("WDebug", "Unable to create JSONObject from file (" + testJson.getAbsolutePath() + "): " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
