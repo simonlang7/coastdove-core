229f2c0ded219a73fe3676031c53c427180b21b2
Simon Lang <simon.lang7@gmail.com>
2016-05-19 00:55:24 +0200

    Some experimental stuff

--- app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
@@ -5,25 +5,34 @@ import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.ComponentName;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 import android.util.Pair;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * Created by simon on 20/01/16.
  */
 public class DetectAppScreenAccessibilityService extends AccessibilityService {
+
+    protected static final int TIME_BETWEEN_LAYOUT_COMPARISONS = 500;
+
+    protected List<Pair<String, JSONObject>> screenDefinitions = null;
+    protected long timeOfLastLayoutComparison = 0;
 
     private ActivityInfo tryGetActivity(ComponentName componentName) {
         try {
@@ -33,9 +42,59 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
         }
     }
 
+    protected boolean shallPerformLayoutComparison(AccessibilityNodeInfo source) {
+        return source != null
+                && System.currentTimeMillis() - timeOfLastLayoutComparison >= TIME_BETWEEN_LAYOUT_COMPARISONS;
+    }
+
+    protected AccessibilityNodeInfo getIdContentNodeInfo(AccessibilityNodeInfo sourceNodeInfo) {
+        AccessibilityNodeInfo currentNodeInfo = sourceNodeInfo;
+
+        // Try to find id/content by traversing the tree upwards
+        while (currentNodeInfo.getParent() != null) {
+            String resourceName = currentNodeInfo.getViewIdResourceName();
+            if (resourceName != null && resourceName.equals("android:id/content")) {
+                // Found it
+                return currentNodeInfo;
+            }
+            currentNodeInfo = currentNodeInfo.getParent();
+        }
+
+        // Traverse tree downwards (breadth-first search)
+        List<AccessibilityNodeInfo> nodeInfos = new LinkedList<>();
+        nodeInfos.add(currentNodeInfo); // currently the root of the tree
+        while (nodeInfos.size() > 0) {
+            currentNodeInfo = nodeInfos.get(0);
+            String resourceName = currentNodeInfo.getViewIdResourceName();
+            if (resourceName != null && resourceName.equals("android:id/content")) {
+                // Found it
+                return currentNodeInfo;
+            }
+            else {
+                // Add all child nodes
+                for (int i = 0; i < currentNodeInfo.getChildCount(); ++i)
+                    nodeInfos.add(currentNodeInfo.getChild(i));
+            }
+            nodeInfos.remove(0);
+        }
+
+        return null;
+    }
+
+    protected String compareLayouts(AccessibilityNodeInfo androidIdContent) {
+        if (androidIdContent.getChildCount() < 1)
+            return "Unknown screen";
+
+
+
+        return "";
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         if (event.getPackageName() != null) {
+
+            // New activity?
             if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
                 ComponentName componentName = new ComponentName(
                         event.getPackageName().toString(),
@@ -47,11 +106,38 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
                 if (isActivity)
                     Log.i("CurrentActivity", componentName.flattenToShortString());
             }
+//            else if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED && event.getSource() != null) {
+//                if (event.getSource().getText() != null)
+//                    Log.i("View clicked", event.getSource().getText().toString());
+//                else if (event.getSource().getViewIdResourceName() != null)
+//                    Log.i("View clicked", event.getSource().getViewIdResourceName());
+//            }
+//            else if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_SELECTED && event.getSource() != null) {
+//                if (event.getSource().getText() != null)
+//                    Log.i("View selected", event.getSource().getText().toString());
+//                else if (event.getSource().getViewIdResourceName() != null)
+//                    Log.i("View selected", event.getSource().getViewIdResourceName());
+//            }
+//            else if (event.getEventType() != AccessibilityEvent.TYPE_VIEW_SCROLLED && event.getSource() != null) {
+//                if (event.getSource().getText() != null)
+//                    Log.i("Type", event.getEventType() + ", Text: " + event.getSource().getText().toString());
+//                else if (event.getSource().getViewIdResourceName() != null)
+//                    Log.i("Type", event.getEventType() + ", ViewIdResName: " + event.getSource().getViewIdResourceName());
+//            }
+
+            AccessibilityNodeInfo sourceNodeInfo = event.getSource();
+            if (shallPerformLayoutComparison(sourceNodeInfo)) {
+                AccessibilityNodeInfo idContentNodeInfo = getIdContentNodeInfo(sourceNodeInfo);
+
+                timeOfLastLayoutComparison = System.currentTimeMillis();
+            }
+
 
             if (event.getPackageName().toString().equalsIgnoreCase("com.whatsapp")) {
 
             }
         }
+//        Log.i("AccessibilityEvent", "" + event.getEventType());
     }
 
     @Override
@@ -60,14 +146,50 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
 
         //Configure these here for compatibility with API 13 and below.
         AccessibilityServiceInfo config = new AccessibilityServiceInfo();
-        config.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
-        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+//        config.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+//        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        config.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+        //config.flags = AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS | AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+        //config.flags = AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS;
 
-        if (Build.VERSION.SDK_INT >= 16)
-            //Just in case this helps
-            config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+//        if (Build.VERSION.SDK_INT >= 16)
+//            //Just in case this helps
+//            config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
 
         setServiceInfo(config);
+
+
+        // TODO: temporary solution: read one json file for comparison
+        screenDefinitions = new LinkedList<>();
+        File[] testJsons = new File[6];
+        testJsons[0] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity.json");
+        testJsons[1] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity_fragment_description.json");
+        testJsons[2] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity_fragment_preview_dialog.json");
+        testJsons[3] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity_fragment_protection_switch.json");
+        testJsons[4] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity_fragment_statistics.json");
+        testJsons[5] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "main_activity_popup_instructions.json");
+
+        for (File testJson : testJsons) {
+            try (InputStream is = new FileInputStream(testJson);
+                 InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader br = new BufferedReader(isr)) {
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
+                }
+                screenDefinitions.add(new Pair<>(testJson.getName(), new JSONObject(stringBuilder.toString())));
+            } catch (FileNotFoundException e) {
+                Log.e("WDebug", "File not found: " + testJson.getAbsolutePath());
+                Log.e("WDebug", e.getMessage());
+            } catch (JSONException e) {
+                Log.e("WDebug", "Unable to create JSONObject from file (" + testJson.getAbsolutePath() + "): " + e.getMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
--- app/src/main/java/de/uni_bonn/detectappscreen/InfoActivity.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/InfoActivity.java
@@ -23,44 +23,13 @@ import java.util.List;
 
 public class InfoActivity extends AppCompatActivity {
 
-    protected static List<Pair<String, JSONObject>> screenDefinitions = null;
-
-    public static List<Pair<String, JSONObject>> getScreenDefinitions() {
-        return new LinkedList<>(screenDefinitions);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
 
-        // TODO: temporary solution: read one json file for comparison
-        screenDefinitions = new LinkedList<>();
-        File[] testJsons = new File[2];
-        testJsons[0] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "home.json");
-        testJsons[1] = new File(Environment.getExternalStoragePublicDirectory("DetectAppScreen"), "conversation.json");
 
-        for (File testJson : testJsons) {
-            try (InputStream is = new FileInputStream(testJson);
-                 InputStreamReader isr = new InputStreamReader(is);
-                 BufferedReader br = new BufferedReader(isr)) {
-                String line = "";
-                StringBuilder stringBuilder = new StringBuilder();
-                while ((line = br.readLine()) != null) {
-                    stringBuilder.append(line);
-                    stringBuilder.append("\n");
-                }
-                InfoActivity.screenDefinitions.add(new Pair<>(testJson.getName(), new JSONObject(stringBuilder.toString())));
-            } catch (FileNotFoundException e) {
-                Log.e("WDebug", "File not found: " + testJson.getAbsolutePath());
-                Log.e("WDebug", e.getMessage());
-            } catch (JSONException e) {
-                Log.e("WDebug", "Unable to create JSONObject from file (" + testJson.getAbsolutePath() + "): " + e.getMessage());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
     }
 }
