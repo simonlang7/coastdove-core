1f31605a06a0f8fbbb7f36ec61951750516156bd
Simon Lang <simon.lang7@gmail.com>
2016-06-17 19:38:06 +0200

    Split stuff into functions

--- app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
+++ app/src/main/java/de/uni_bonn/detectappscreen/DetectAppScreenAccessibilityService.java
@@ -35,7 +35,7 @@ import java.util.TreeSet;
  */
 public class DetectAppScreenAccessibilityService extends AccessibilityService {
 
-    protected static final int TIME_BETWEEN_LAYOUT_COMPARISONS = 500;
+    protected static final int TIME_BETWEEN_LAYOUT_COMPARISONS = 100;
 
     protected List<Pair<String, JSONObject>> screenDefinitions = null;
     protected long timeOfLastLayoutComparison = 0;
@@ -57,151 +57,102 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
                 && System.currentTimeMillis() - timeOfLastLayoutComparison >= TIME_BETWEEN_LAYOUT_COMPARISONS;
     }
 
-    protected AccessibilityNodeInfo getIdContentNodeInfo(AccessibilityNodeInfo sourceNodeInfo) {
+    protected AccessibilityNodeInfo getRootNodeInfo(AccessibilityNodeInfo sourceNodeInfo) {
         AccessibilityNodeInfo currentNodeInfo = sourceNodeInfo;
 
-        // Try to find id/content by traversing the tree upwards
-        while (currentNodeInfo != null && currentNodeInfo.getParent() != null) {
-            String resourceName = currentNodeInfo.getViewIdResourceName();
-            if (resourceName != null && resourceName.equals("android:id/content")) {
-                // Found it
-                return currentNodeInfo;
-            }
+        while (currentNodeInfo != null && currentNodeInfo.getParent() != null)
             currentNodeInfo = currentNodeInfo.getParent();
-        }
 
-        AccessibilityNodeInfo rootNodeInfo = currentNodeInfo;
+        return currentNodeInfo;
+    }
+
+    protected void checkActivity(AccessibilityEvent event) {
+        // New activity?
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            ComponentName componentName = new ComponentName(
+                    event.getPackageName().toString(),
+                    event.getClassName().toString()
+            );
+
+            ActivityInfo activityInfo = tryGetActivity(componentName);
+            boolean isActivity = activityInfo != null;
+            if (isActivity)
+                Log.i("CurrentActivity", componentName.flattenToShortString());
+        }
+    }
+
+    protected Set<String> androidIDsOnScreen(AccessibilityNodeInfo sourceNodeInfo) {
+        AccessibilityNodeInfo currentNodeInfo = getRootNodeInfo(sourceNodeInfo);
+        Set<String> androidIDsOnScreen = new TreeSet<>(Collator.getInstance());
 
         // Traverse tree downwards (breadth-first search)
         List<AccessibilityNodeInfo> nodeInfos = new LinkedList<>();
         nodeInfos.add(currentNodeInfo); // currently the root of the tree
         while (nodeInfos.size() > 0) {
             currentNodeInfo = nodeInfos.get(0);
-            if (currentNodeInfo == null) {
-                nodeInfos.remove(0);
-                continue;
-            }
-            String resourceName = currentNodeInfo.getViewIdResourceName();
-            if (resourceName != null && resourceName.equals("android:id/content")) {
-                // Found it
-                return currentNodeInfo;
-            }
-            else {
-                // Add all child nodes
+            if (currentNodeInfo != null) {
+                String viewIdResName = currentNodeInfo.getViewIdResourceName();
+                String currentAndroidID = viewIdResName != null ? viewIdResName.replace("com.whatsapp:", "") : "";
+                if (!currentAndroidID.equals("")) {
+                    androidIDsOnScreen.add(currentAndroidID);
+                }
+
+                // add children
                 for (int i = 0; i < currentNodeInfo.getChildCount(); ++i)
                     nodeInfos.add(currentNodeInfo.getChild(i));
             }
+
+
             nodeInfos.remove(0);
         }
-
-        return rootNodeInfo;
     }
 
-    protected String compareLayouts(AccessibilityNodeInfo androidIdContent) {
-        if (androidIdContent.getChildCount() < 1)
-            return "Unknown screen";
+    protected Set<String> possibleLayouts(Set<String> androidIDs) {
+        Set<String> possibleLayouts = new TreeSet<>(Collator.getInstance());
+        for (String androidIDOnScreen : androidIDs) {
+            Set<String> currentPossibleLayouts = this.reverseMap.get(androidIDOnScreen);
+            if (currentPossibleLayouts != null)
+                possibleLayouts.addAll(currentPossibleLayouts);
+        }
 
+        return possibleLayouts;
+    }
 
+    protected Set<String> recognizedLayouts(Set<String> androidIDs, Set<String> possibleLayouts) {
+        Set<String> recognizedLayouts = new TreeSet<>(Collator.getInstance());
+        for (String possibleLayout : possibleLayouts) {
+            LayoutIdentification layout = this.layoutIdentificationMap.get(possibleLayout);
+            for (Set<String> layoutIdentifierSet : layout.getLayoutIdentifiers()) {
+                if (androidIDs.containsAll(layoutIdentifierSet)) {
+                    recognizedLayouts.add(layout.getName());
+                    break;
+                }
+            }
+        }
+    }
 
-        return "";
+    protected void checkLayout(AccessibilityEvent event) {
+        AccessibilityNodeInfo sourceNodeInfo = event.getSource();
+
+        if (shallPerformLayoutComparison(sourceNodeInfo)) {
+
+            if (event.getPackageName().toString().equalsIgnoreCase("com.whatsapp")) {
+                Set<String> androidIDsOnScreen = androidIDsOnScreen(sourceNodeInfo);
+                Set<String> possibleLayouts = possibleLayouts(androidIDsOnScreen);
+                Set<String> recognizedLayouts = recognizedLayouts(androidIDsOnScreen, possibleLayouts);
+
+                Log.i("Recognized Layouts: ", recognizedLayouts.toString());
+            }
+
+            timeOfLastLayoutComparison = System.currentTimeMillis();
+        }
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         if (event.getPackageName() != null) {
-
-            // New activity?
-            if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-                ComponentName componentName = new ComponentName(
-                        event.getPackageName().toString(),
-                        event.getClassName().toString()
-                );
-
-                ActivityInfo activityInfo = tryGetActivity(componentName);
-                boolean isActivity = activityInfo != null;
-                if (isActivity)
-                    Log.i("CurrentActivity", componentName.flattenToShortString());
-            }
-//            else if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED && event.getSource() != null) {
-//                if (event.getSource().getText() != null)
-//                    Log.i("View clicked", event.getSource().getText().toString());
-//                else if (event.getSource().getViewIdResourceName() != null)
-//                    Log.i("View clicked", event.getSource().getViewIdResourceName());
-//            }
-//            else if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_SELECTED && event.getSource() != null) {
-//                if (event.getSource().getText() != null)
-//                    Log.i("View selected", event.getSource().getText().toString());
-//                else if (event.getSource().getViewIdResourceName() != null)
-//                    Log.i("View selected", event.getSource().getViewIdResourceName());
-//            }
-//            else if (event.getEventType() != AccessibilityEvent.TYPE_VIEW_SCROLLED && event.getSource() != null) {
-//                if (event.getSource().getText() != null)
-//                    Log.i("Type", event.getEventType() + ", Text: " + event.getSource().getText().toString());
-//                else if (event.getSource().getViewIdResourceName() != null)
-//                    Log.i("Type", event.getEventType() + ", ViewIdResName: " + event.getSource().getViewIdResourceName());
-//            }
-
-            AccessibilityNodeInfo sourceNodeInfo = event.getSource();
-            if (shallPerformLayoutComparison(sourceNodeInfo)) {
-                AccessibilityNodeInfo currentNodeInfo = getIdContentNodeInfo(sourceNodeInfo);
-
-                if (event.getPackageName().toString().equalsIgnoreCase("com.whatsapp")) {
-                    Set<String> androidIDsOnScreen = new TreeSet<>(Collator.getInstance());
-                    Set<String> recognizedLayouts = new TreeSet<>(Collator.getInstance());
-                    Set<String> possibleLayouts = new TreeSet<>(Collator.getInstance());
-
-                    // Traverse tree downwards (breadth-first search)
-                    List<AccessibilityNodeInfo> nodeInfos = new LinkedList<>();
-                    nodeInfos.add(currentNodeInfo); // currently the root of the tree
-                    while (nodeInfos.size() > 0) {
-                        currentNodeInfo = nodeInfos.get(0);
-                        if (currentNodeInfo != null) {
-                            String viewIdResName = currentNodeInfo.getViewIdResourceName();
-                            String currentAndroidID = viewIdResName != null ? viewIdResName.replace("com.whatsapp:", "") : "";
-                            if (!currentAndroidID.equals("")) {
-                                androidIDsOnScreen.add(currentAndroidID);
-                            }
-
-                            // add children
-                            for (int i = 0; i < currentNodeInfo.getChildCount(); ++i)
-                                nodeInfos.add(currentNodeInfo.getChild(i));
-                        }
-
-
-                        nodeInfos.remove(0);
-                    }
-
-                    for (String androidIDOnScreen : androidIDsOnScreen) {
-                        Set<String> currentPossibleLayouts = this.reverseMap.get(androidIDOnScreen);
-                        if (currentPossibleLayouts != null)
-                            possibleLayouts.addAll(currentPossibleLayouts);
-                    }
-
-//                    StringBuilder sb = new StringBuilder();
-//                    for (String st : this.reverseMap.keySet())
-//                        sb.append(st + " ");
-//
-//                    Log.i("ReverseMap KeySet", sb.toString());
-//                    Log.i("AndroidIDs on Screen", androidIDsOnScreen.toString());
-//                    Log.i("WDebug", "POSSIBLE LAYOUTS");
-                    for (String possibleLayout : possibleLayouts) {
-//                        Log.i("WDebug", "possible Layout: " + possibleLayout);
-                        LayoutIdentification layout = this.layoutIdentificationMap.get(possibleLayout);
-                        for (Set<String> layoutIdentifierSet : layout.getLayoutIdentifiers()) {
-                            if (androidIDsOnScreen.containsAll(layoutIdentifierSet)) {
-                                recognizedLayouts.add(layout.getName());
-                                break;
-                            }
-                        }
-                    }
-
-                    Log.i("Recognized Layouts: ", recognizedLayouts.toString());
-                }
-
-                timeOfLastLayoutComparison = System.currentTimeMillis();
-            }
-
-
+            checkActivity(event);
+            checkLayout(event);
         }
 //        Log.i("AccessibilityEvent", "" + event.getEventType());
     }
@@ -262,9 +213,6 @@ public class DetectAppScreenAccessibilityService extends AccessibilityService {
         }
 
         Log.i("WDebug", "Done building Hashmaps.");
-
-        // debug test
-        //Log.i("WDebug", "layouts[abc_action_bar_title_item] = " + layoutIdentificationMap.get("abc_action_bar_title_item").getLayoutIdentifiers().toString());
     }
 
     public JSONObject readJSONFile(String filename) {
